9422efe9b1613e1dd46e034741440ee553b6e3b5
chuvak393 <chuvak393@gmail.com>
2020-04-22 22:26:06 +0400

    feat: fix play mode

--- src/ts/play.ts
+++ src/ts/play.ts
@@ -1,11 +1,6 @@
 import Sound from "./audio";
 import cards from "./cards";
 
-// const switch: HTMLDivElement = document.querySelector('.switch-btn');
-
-// console.log(document.querySelector('.switch-btn'))
-// console.log(document.querySelector("body > main > section > div > div > div.category > h1"))
-
 export function game() {
   if (document.querySelector('.switch-btn')?.classList.contains('switch-on')) {
     let category: string = document.querySelector('.category_text')?.innerHTML;
@@ -13,10 +8,9 @@ export function game() {
     let sound = new Sound(category)
     let audioList = sound.soundGenerate()
     let i: number = 0;
+    let errorCounter: number = 0;
     let currentAudio = sound.soundPlay(audioList, i)
     button.onclick = () => {
-      // currentAudio.play()
-      // i += 1;
       currentAudio = sound.soundPlay(audioList, i)
       if (!button.classList.contains('button-repeat')) {
         currentAudio.play()
@@ -24,16 +18,28 @@ export function game() {
       }
       document.querySelectorAll('.card').forEach((card) => {
         card.onclick = (event) => {
-          let currentCard = event.target.closest('.card').id;
-          let path = cards[category].find(item => item.word === `${currentCard}`);
-          // console.log(path.audio)
-          // console.log(audioList[i])
-          if (path.audio === audioList[i]) {
-            console.log('УРА')
-            i += 1;
-          }
-          else {
-            console.log('НЕ УРА')
+          let currentCard = event.target.closest('.card');
+          let path = cards[category].find(item => item.word === `${currentCard.id}`);
+
+          if (!currentCard.classList.contains('card_correct')) {
+            if (path.audio === audioList[i]) {
+              let correct = new Audio('./audio/correct.mp3');
+              correct.play();
+              currentCard.classList.add('card_correct');
+              i += 1;
+              currentAudio = sound.soundPlay(audioList, i);
+              setTimeout(() => currentAudio.play(), 1000)
+            }
+            else {
+              let error = new Audio('./audio/error.mp3')
+              error.play()
+              errorCounter += 1;
+            }
+            if (i > 7) {
+              let success = new Audio('./audio/success.mp3')
+              success.play()
+              console.log(`Ошибок: ${errorCounter}`)
+            }
           }
         }
       })
@@ -42,8 +48,3 @@ export function game() {
 };
 
 
-
-// document.addEventListener('click', play)
-
-// console.log(document.querySelector('.category_text')?.innerHTML)
-// console.log(document.querySelector('.category_text'))
